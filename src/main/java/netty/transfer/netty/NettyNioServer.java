011d8543fd8b000a47598a0516997974076bdb0c
qrn <miraitowa1796@163.com>
2021-06-29 16:24:50 +0800

    netty

--- src/main/java/netty/transfer/netty/NettyNioServer.java
+++ src/main/java/netty/transfer/netty/NettyNioServer.java
@@ -5,10 +5,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.channel.socket.oio.OioServerSocketChannel;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
